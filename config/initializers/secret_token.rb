db6a4f37624032480ef6dccd09c473814ab08c93
gogostella <stella@indiegogo.com>
2015-04-26 14:57:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmojiParty::Application.config.secret_token = '901d45ce4d6bfac219346fbba0ac78795c99b77b48d29a067e70da390fd1a28bf048f2a5925ec8124cdd23589b33973a8dd62493b6aa35a598d95a35cfe584f8'
