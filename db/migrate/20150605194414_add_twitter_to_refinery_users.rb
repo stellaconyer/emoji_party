1c4aa87bd0c70d6898dcb0ff0f7b537bc4d730f1
Siena Aguayo <siena.aguayo@gmail.com>
2015-06-05 23:04:58 -0700

    added author sidebar

--- db/migrate/20150605194414_add_twitter_to_refinery_users.rb
+++ db/migrate/20150605194414_add_twitter_to_refinery_users.rb
@@ -1,6 +1,6 @@
 # This migration comes from refinery_authentication (originally 20120301234455)
 class AddTwitterToRefineryUsers < ActiveRecord::Migration
   def change
-    add_column :refinery_user, :twitter, :string
+    add_column :refinery_users, :twitter, :string
   end
 end
