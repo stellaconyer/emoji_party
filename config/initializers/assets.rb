e199c436b6d699aae6a227a0b1aea782f7b88b8d
Siena Aguayo <siena.aguayo@gmail.com>
2015-09-26 12:33:42 -0700

    generate meta tag for og:image on document ready

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( refinery/blog/frontend.css )
+Rails.application.config.assets.precompile += %w( generate_ogimage.js )
