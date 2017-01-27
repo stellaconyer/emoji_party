46d8190202d09d4fe93148edda61c79c193c524e
Siena Aguayo <siena.aguayo@gmail.com>
2017-01-27 13:32:45 -0800

    update speakerdeck link from sienaigg => sienatime

--- script/siena_talks.rb
+++ script/siena_talks.rb
@@ -7,7 +7,7 @@ realm_talk = Talk.create!(
   refinery_user: siena,
   title: "Gotta Persist 'Em All: Realm as Replacement for SQLite",
   description: "Realm is a new player on mobile database scene, and acts as a replacement for SQLite on Android and Core Data on iOS by providing a database and an easy-to-use object-relational mapping-like (ORM) interface. Learn how to use it in your own projects and how it compares to working with SQLite in a beginner-friendly sample app with Pokémon.",
-  slides_link: "https://speakerdeck.com/sienaigg/gotta-persist-em-all-realm-as-replacement-for-sqlite"
+  slides_link: "https://speakerdeck.com/sienatime/gotta-persist-em-all-realm-as-replacement-for-sqlite"
   )
 
 Appearance.create!(
@@ -30,7 +30,7 @@ i18n_talk = Talk.create!(
   refinery_user: siena,
   title: "Fearless Internationalization and Localization Across the Nations",
   description: "Many people are intimidated by localization (l10n) and internationalization (i18n) but they don’t have to be—Android provides many tools out of the box to aide you in global domination. Learn what to watch out for while coding your app that will make your life easier once you receive localized content. We’ll cover alternate resources, strings files, date and number formatters and other tips from Android Studio, Google, and third parties that will turn you into an i18n and l10n champion.",
-  slides_link: "https://speakerdeck.com/sienaigg/fearless-internationalization-and-localization-across-the-nations"
+  slides_link: "https://speakerdeck.com/sienatime/fearless-internationalization-and-localization-across-the-nations"
   )
 
 Appearance.create!(
